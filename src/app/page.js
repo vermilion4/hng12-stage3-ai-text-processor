5ad1ce43e65c75dabd9e9054f563c56bf9496f3d
Adaeze Ndupu <andupu4@gmail.com>
2025-02-17 20:37:40 +0100

    chore: test translator

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,27 @@
+"use client";
+import { useEffect } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  
+  useEffect(() => {
+    async function initTranslator() {
+      const translator = await self.ai.translator.create({
+        sourceLanguage: 'en',
+        targetLanguage: 'fr',
+        monitor(m) {
+          m.addEventListener('downloadprogress', (e) => {
+            console.log(`Downloaded ${e.loaded} of ${e.total} bytes.`);
+          });
+        },
+      });
+
+      const result = await translator.translate('Hello, world!');
+      console.log(result);
+    }
+    initTranslator();
+  }, []);
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
